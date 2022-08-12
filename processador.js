c337a6e02ee68680573fe34c2dc402f7f31576df
Maria Silvia <mariasrr97@gmail.com>
2022-08-12 16:37:43 -0300

    add styles and draw initial line

--- processador.js
+++ processador.js
@@ -1,34 +1,30 @@
-function load_reta_inicial () {
-  alert("You are visiting the home page of \n" +
-      "Pete's Pickled Peppers \n" + "WELCOME!!!");
-  
-   var canvas = document.getElementById("canvas");
+function load_reta_inicial() {
+  var canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "rgba(148, 55, 171, 0.40)";
+  var width = canvas.width;
+  var height = canvas.height;
 
+  let middle_h = height/2
+  let middle_w = width/2
+  
+  ctx.beginPath();
+  let lineSize = 100
+  ctx.moveTo(middle_w, middle_h-lineSize);
+  ctx.lineTo(middle_w, middle_h+lineSize);
+  ctx.stroke()
 }
 
 const processadorRetas = {
-    myProperty: 'someValue',
-    // object literals can contain properties and methods.
-    // e.g we can define a further object for module configuration:
-    myConfig: {
-        useCaching: true,
-        language: 'en',
-    },
-    // a very basic method
-    saySomething() {
-        console.log('Where is Paul Irish debugging today?');
-    },
-    // output a value based on the current configuration
-    reportMyConfig() {
-        console.log(
-            `Caching is: ${this.myConfig.useCaching ? 'enabled' : 'disabled'}`
-        );
-    },
-    // override the current configuration
-    updateMyConfig(newConfig) {
-        if (typeof newConfig === 'object') {
-            this.myConfig = newConfig;
-            console.log(this.myConfig.language);
-        }
-    },
+  myProperty: "someValue",
+  // object literals can contain properties and methods.
+  // e.g we can define a further object for module configuration:
+  myConfig: {
+    useCaching: true,
+    language: "en",
+  },
+
+  moverReta() {
+    console.log("Where is Paul Irish debugging today?");
+  },
 };
